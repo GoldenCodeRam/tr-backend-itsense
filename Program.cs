b9067d1a2c219200975960f3ae4abf5542a6e36d
Luis Alejandro Quiroga Gómez <lualquigo@gmail.com>
2023-01-27 10:41:17 -0500

    Add: CORS access for development with frontend

--- Program.cs
+++ Program.cs
@@ -3,24 +3,35 @@ using Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options => {
+  options.AddDefaultPolicy(builder => {
+    builder.AllowAnyHeader();
+    builder.AllowAnyMethod();
+    builder.SetIsOriginAllowed(
+        origin => { return new Uri(origin).Host == "localhost"; });
+  });
+});
+
 // Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at
+// https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<ProductContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("TestDB")
-));
+builder.Services.AddDbContext<ProductContext>(
+    options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("TestDB")));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+if (app.Environment.IsDevelopment()) {
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseAuthorization();
 
